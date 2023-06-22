b709d6155bbb32cdd4ea6c4ebd819a14f2f585c6
Venkata sai <116934116+venkat93478@users.noreply.github.com>
2023-06-22 20:40:55 +0530

    Update Primitivedatatypes.java

--- Primitivedatatypes.java
+++ Primitivedatatypes.java
@@ -1,1 +1,24 @@
+package coreJava;
 
+public class primitiveDT {
+	public static void main(String [] args) {
+		int x = 5;
+		int y = 23;
+		System.out.println("Value of x:"+x);
+		System.out.println("Value of y:"+y);
+		
+		byte b = (byte)178;
+		System.out.println(b);
+		
+		int i = b ;
+		System.out.println(i);
+		
+		float a = 10;
+		double d = 1232;
+		double d1 = ++d;
+		
+		double result = a + d1;
+		System.out.println(result);
+	}
+
+}
